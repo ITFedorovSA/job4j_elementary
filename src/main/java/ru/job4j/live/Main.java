605bc427b126b4bf518964db3c0391b90c8e47c3
FedorovSA <itfedorovsa@gmail.com>
2022-03-27 13:34:56 +0300

    Main.java

---
+++ src/main/java/ru/job4j/live/Main.java
@@ -0,0 +1,10 @@
+package ru.job4j.live;
+
+public class Main {
+    public static void main(String[] args) { //psvm
+        System.out.println();                //sout
+        for (int i = 0; i < 10; i++) {       //fori
+            System.out.println();
+        }
+    }
+}
