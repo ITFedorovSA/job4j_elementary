8404198d588e0b46d8d3c914124609dfbd3ce6b6
FedorovSA <itfedorovsa@gmail.com>
2022-04-04 20:13:07 +0300

    MatrixCheck - update

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,10 +2,10 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
-        boolean result = false;
+        boolean result = true;
         for (int index = 0; index < board.length; index++) {
             if (board[row][index] != 'X') {
-                result = true;
+                result = false;
                 break;
             }
         }
