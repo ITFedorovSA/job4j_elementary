03ffb0a971c735ba4d773f610cac7b90f63eba88
FedorovSA <itfedorovsa@gmail.com>
2022-06-08 12:15:45 +0300

    FileOutputStream - matrix (update)

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Matrix {
@@ -15,10 +16,13 @@ public class Matrix {
     }
 
     public static void main(String[] args) {
+        int[][] array = multiple(9);
         try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
-            out.write(Arrays.deepToString(multiple(9)).getBytes());
-            out.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+            for (int[] el : array) {
+                out.write(Arrays.toString(el).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
